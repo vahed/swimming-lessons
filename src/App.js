89b74b8314df24bd16e48eb9df5fabfa74b4fec3
vahed-dwell <59022473+vahed-dwell@users.noreply.github.com>
2020-09-05 11:03:11 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -18,33 +18,28 @@ import ContactForm from "./components/contactForm";
 function App() {
   return (
       <div className="grid-container">
-        <div className="grid-item item1"><WebNavbar /><ImageCarousel /></div>
-        <div className="grid-item item2"></div>
-        <div className="grid-item item3"><Introduction/></div>
-        <div className="grid-item item4"></div>
+            <div className="grid-item item1"><span id="webNav"><WebNavbar /></span></div>
+        <div className="grid-item item2"><ImageCarousel /></div>
+
+        <div className="grid-item item3"></div>
+        <div className="grid-item item4"><span id="intro"><Introduction /></span></div>
         <div className="grid-item item5"></div>
-        <div className="grid-item item6"><Body /></div>
-        <div className="grid-item item7"></div>
+
+            <div className="grid-item item6"></div>
+            <div className="grid-item item7"><span id="bdy"><Body /></span></div>
         <div className="grid-item item8"></div>
-        <div className="grid-item item9"><Stage /></div>
-        <div className="grid-item item10"></div>
-        <div className="grid-item item11"><ImageCrossFade />
-        </div>
-        <div className="grid-item item12"></div>
+        <div className="grid-item item9"></div>
+            <div className="grid-item item10"><span id="stg"><Stage /></span></div>
+        <div className="grid-item item11"></div>
+            <div className="grid-item item12"><ImageCrossFade /></div>
         <div className="grid-item item13"></div>
         <div className="grid-item item14"></div>
-        <div className="grid-item item15"></div>
+            <div className="grid-item item15"><span id="cform"><ContactForm/></span></div>
         <div className="grid-item item16"></div>
-        <div className="grid-item item17"><ContactForm/></div>
+            <div className="grid-item item17"><span id="loc"><Map/></span></div>
         <div className="grid-item item18"></div>
-        <div className="grid-item item19">
-              <Map/>
-        </div>
+        <Footer/>
         <div className="grid-item item20"></div>
-        <div className="grid-item item21">
-            <Footer/>
-        </div>
-        <div className="grid-item item22"></div>
     </div>
   );
 }
